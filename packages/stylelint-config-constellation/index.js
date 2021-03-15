a2e95a50245d53a6c233ca64c450c893fbad9c5c
Pedro Filho <pedro@filho.me>
2021-03-15 11:05:53 -0300

    Adding prettier to stylelint config

--- packages/stylelint-config-constellation/index.js
+++ packages/stylelint-config-constellation/index.js
@@ -1,13 +1,16 @@
 module.exports = {
   processors: ["stylelint-processor-styled-components"],
+  plugins: ["stylelint-prettier"],
   extends: [
     "stylelint-config-recommended",
     "stylelint-config-styled-components",
+    "stylelint-config-prettier",
   ],
   rules: {
     "property-no-vendor-prefix": null,
     "media-feature-name-disallowed-list": ["max-width", "min-width"],
     "at-rule-disallowed-list": ["media"],
     "no-descending-specificity": null,
+    "prettier/prettier": true,
   },
 };
