d03b519cd612c9ff720bc4a39f0ce7b6fc390d33
Diego Sisto <dsisto@blockchain.com>
2021-05-26 08:32:27 -0300

    Allow at media, max-width and min-width

--- packages/stylelint-config-constellation/index.js
+++ packages/stylelint-config-constellation/index.js
@@ -7,8 +7,6 @@ module.exports = {
     "stylelint-config-styled-components",
   ],
   rules: {
-    "at-rule-disallowed-list": ["media"],
-    "media-feature-name-disallowed-list": ["max-width", "min-width"],
     "no-descending-specificity": null,
     "prettier/prettier": true,
     "property-no-vendor-prefix": null,
