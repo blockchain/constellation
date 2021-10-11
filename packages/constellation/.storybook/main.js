d2de47591956343eb9b402944a25d45b78a2dba9
Pedro Filho <pedro@filho.me>
2021-10-11 14:25:58 +0100

    feat(tracking): adding tracking function

--- packages/constellation/.storybook/main.js
+++ packages/constellation/.storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
-  "stories": [
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 }
