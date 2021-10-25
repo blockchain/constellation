c32f8f30537a0074227d06534078de38d38f878e
Pedro Filho <pedro@filho.me>
2021-10-25 15:00:28 +0100

    feat(colors): adding colors

--- packages/constellation/src/colors/index.ts
+++ packages/constellation/src/colors/index.ts
@@ -1,7 +1,23 @@
 import blue from './blue'
+import dark from './dark'
+import green from './green'
+import grey from './grey'
+import orange from './orange'
+import purple from './purple'
+import red from './red'
+import teal from './teal'
+import white from './white'
 
 const colors = {
-  blue
+  blue,
+  dark,
+  green,
+  grey,
+  orange,
+  purple,
+  red,
+  teal,
+  white
 }
 
 export default colors
---
+++ packages/constellation/src/colors/red.ts
@@ -0,0 +1,14 @@
+const red = {
+  0: '#FFD9D6',
+  100: '#FFECEB',
+  200: '#FFC3BF',
+  300: '#F6A7A1',
+  400: '#FF3344',
+  500: '#EA1F30',
+  600: '#CF1726',
+  700: '#A50D0D',
+  800: '#8D0B0B',
+  900: '#790606'
+} as const
+
+export default red
