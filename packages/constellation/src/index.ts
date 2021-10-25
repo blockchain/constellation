b0c6254c441eda022beef131fffb5748cb554390
Pedro Filho <pedro@filho.me>
2021-10-25 15:27:56 +0100

    feat(colors): adding export for colors

--- packages/constellation/src/index.ts
+++ packages/constellation/src/index.ts
@@ -1,4 +1,5 @@
+import colors from './colors'
 import { tracking } from './services'
 import { debounce, parseSymbol, parseUTM, queuevent, utmCampaignBuilder } from './utils'
 
-export { debounce, parseSymbol, parseUTM, queuevent, tracking, utmCampaignBuilder }
+export { colors, debounce, parseSymbol, parseUTM, queuevent, tracking, utmCampaignBuilder }
