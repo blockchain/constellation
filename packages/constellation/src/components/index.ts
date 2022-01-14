6bc796a22fa1de312973df923efd384be5da88d0
Pedro Filho <pedro@filho.me>
2022-01-14 14:59:56 +0000

    feat(phase0): removing everything but text

--- packages/constellation/src/components/index.ts
+++ packages/constellation/src/components/index.ts
@@ -1,6 +1,3 @@
-import CopyToClipboard from './CopyToClipboard'
-import Icon, { IconName } from './Icon'
-import Switch from './Switch'
 import Text from './Text'
 
-export { CopyToClipboard, Icon, IconName, Switch, Text }
+export { Text }
