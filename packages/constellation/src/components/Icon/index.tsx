1a2be2cabb7591ecd20c61c1158bb17dc7e10ed4
Andrew Schneider <andrew@blockchain.com>
2022-01-25 08:29:29 -0500

    feat(icons): add icons named D-N

--- packages/constellation/src/components/Icon/index.tsx
+++ packages/constellation/src/components/Icon/index.tsx
@@ -49,6 +49,57 @@ import Components from './icons/components.svg'
 import Computer from './icons/computer.svg'
 import Copy from './icons/copy.svg'
 import CreditCard from './icons/creditCard.svg'
+import Delete from './icons/delete.svg'
+import Deposit from './icons/deposit.svg'
+import Download from './icons/download.svg'
+import Edit from './icons/edit.svg'
+import Education from './icons/education.svg'
+import Email from './icons/email.svg'
+import Error from './icons/error.svg'
+import Exchange from './icons/exchange.svg'
+import ExpandLess from './icons/expandLess.svg'
+import ExpandMore from './icons/expandMore.svg'
+import Explore from './icons/explore.svg'
+import FaceId from './icons/faceId.svg'
+import FavoriteFilled from './icons/favoriteFilled.svg'
+import FavoriteOutline from './icons/favoriteOutline.svg'
+import Filter from './icons/filter.svg'
+import Fingerprint from './icons/fingerprint.svg'
+import Flag from './icons/flag.svg'
+import FlashOff from './icons/flashOff.svg'
+import FlashOn from './icons/flashOn.svg'
+import Fullscreen from './icons/fullscreen.svg'
+import FullscreenExit from './icons/fullscreenExit.svg'
+import Globe from './icons/globe.svg'
+import GoogleTranslate from './icons/googleTranslate.svg'
+import Hardware from './icons/hardware.svg'
+import History from './icons/history.svg'
+import Home from './icons/home.svg'
+import Identification from './icons/identification.svg'
+import Image from './icons/image.svg'
+import Information from './icons/information.svg'
+import Key from './icons/key.svg'
+import Keyboard from './icons/keyboard.svg'
+import Laptop from './icons/laptop.svg'
+import Legal from './icons/legal.svg'
+import LineChartUp from './icons/lineChartUp.svg'
+import Link from './icons/link.svg'
+import ListBullets from './icons/listBullets.svg'
+import LockClosed from './icons/lockClosed.svg'
+import LockOpen from './icons/lockOpen.svg'
+import Logout from './icons/logout.svg'
+import MarketUp from './icons/marketUp.svg'
+import Menu from './icons/menu.svg'
+import Microphone from './icons/microphone.svg'
+import MinusCircle from './icons/minusCircle.svg'
+import MoneyUsd from './icons/moneyUsd.svg'
+import MoreHorizontal from './icons/moreHorizontal.svg'
+import MoreVertical from './icons/moreVertical.svg'
+import NewWindow from './icons/newWindow.svg'
+import Nft from './icons/nft.svg'
+import NotificationOff from './icons/notificationOff.svg'
+import NotificationOn from './icons/notificationOn.svg'
+// import  from './icons/'
 
 // icon component map
 const iconComponentMap: {
@@ -98,6 +149,56 @@ const iconComponentMap: {
   computer: Computer,
   copy: Copy,
   creditCard: CreditCard,
+  delete: Delete,
+  deposit: Deposit,
+  download: Download,
+  edit: Edit,
+  education: Education,
+  email: Email,
+  error: Error,
+  exchange: Exchange,
+  expandLess: ExpandLess,
+  expandMore: ExpandMore,
+  explore: Explore,
+  faceId: FaceId,
+  favoriteFilled: FavoriteFilled,
+  favoriteOutline: FavoriteOutline,
+  filter: Filter,
+  fingerprint: Fingerprint,
+  flag: Flag,
+  flashOff: FlashOff,
+  flashOn: FlashOn,
+  fullscreen: Fullscreen,
+  fullscreenExit: FullscreenExit,
+  globe: Globe,
+  googleTranslate: GoogleTranslate,
+  hardware: Hardware,
+  history: History,
+  home: Home,
+  identification: Identification,
+  image: Image,
+  information: Information,
+  key: Key,
+  keyboard: Keyboard,
+  laptop: Laptop,
+  legal: Legal,
+  lineChartUp: LineChartUp,
+  link: Link,
+  listBullets: ListBullets,
+  lockClosed: LockClosed,
+  lockOpen: LockOpen,
+  logout: Logout,
+  marketUp: MarketUp,
+  menu: Menu,
+  microphone: Microphone,
+  minusCircle: MinusCircle,
+  moneyUsd: MoneyUsd,
+  moreHorizontal: MoreHorizontal,
+  moreVertical: MoreVertical,
+  newWindow: NewWindow,
+  nft: Nft,
+  notificationOff: NotificationOff,
+  notificationOn: NotificationOn,
 }
 
 const iconSizes = {
