2cb1214bc03ecef9387e8f8b546bdb3fafd8e7d1
Andrew Schneider <andrew@blockchain.com>
2022-03-18 10:35:49 -0400

    fix(css): remove global css reset

--- packages/constellation/src/providers/ThemeProvider/ThemeProvider.tsx
+++ packages/constellation/src/providers/ThemeProvider/ThemeProvider.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import { normalize } from 'stitches-normalize-css'
 
-import { globalCss } from '../../stitches.config'
 import { ThemeProviderComponent } from './types'
 
-const normalizeStyles = globalCss(...normalize)
-
-const ThemeProvider: ThemeProviderComponent = ({ children, theme }) => {
-  normalizeStyles()
-
-  return <div className={theme}>{children}</div>
-}
+const ThemeProvider: ThemeProviderComponent = ({ children, theme }) => (
+  <div className={theme}>{children}</div>
+)
 
 export { ThemeProvider }
--- packages/constellation/src/stitches.config.ts
+++ packages/constellation/src/stitches.config.ts
@@ -3,80 +3,79 @@ import { createStitches } from '@stitches/react'
 
 import { colors } from './colors'
 
-const { config, createTheme, css, getCssText, globalCss, keyframes, styled, theme } =
-  createStitches({
-    media: {
-      dark: '(prefers-color-scheme: dark)',
-      hover: '(any-hover: hover)',
-      laptop: '(min-width: 1023px)',
-      light: '(prefers-color-scheme: light)',
-      mobile: '(min-width: 479px)',
-      motion: '(prefers-reduced-motion)',
-      tablet: '(min-width: 767px)',
+const { config, createTheme, css, getCssText, keyframes, styled, theme } = createStitches({
+  media: {
+    dark: '(prefers-color-scheme: dark)',
+    hover: '(any-hover: hover)',
+    laptop: '(min-width: 1023px)',
+    light: '(prefers-color-scheme: light)',
+    mobile: '(min-width: 479px)',
+    motion: '(prefers-reduced-motion)',
+    tablet: '(min-width: 767px)',
+  },
+  theme: {
+    colors: {
+      ...colors,
     },
-    theme: {
-      colors: {
-        ...colors,
-      },
-      fontSizes: {
-        10: '10px',
-        12: '12px',
-        14: '14px',
-        16: '16px',
-        20: '20px',
-        24: '24px',
-        32: '32px',
-        40: '40px',
-      },
-      fonts: {
-        inter:
-          "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
-      },
-      radii: {
-        1: '4px',
-        2: '8px',
-        3: '16px',
-        pill: '20%',
-        round: '50%',
-      },
-      sizes: {
-        16: '16px',
-        24: '24px',
-        32: '32px',
-        4: '4px',
-        40: '40px',
-        48: '48px',
-        64: '64px',
-        72: '64px',
-        8: '8px',
-        80: '80px',
-      },
-      space: {
-        120: '120px',
-        16: '16px',
-        24: '24px',
-        32: '32px',
-        4: '4px',
-        40: '40px',
-        48: '48px',
-        64: '64px',
-        8: '8px',
-        80: '80px',
-      },
-      zIndices: {
-        1: '100',
-        2: '200',
-        3: '300',
-        4: '400',
-        5: '500',
-        6: '600',
-        7: '700',
-        8: '800',
-        9: '900',
-        max: '9999',
-      },
+    fontSizes: {
+      10: '10px',
+      12: '12px',
+      14: '14px',
+      16: '16px',
+      20: '20px',
+      24: '24px',
+      32: '32px',
+      40: '40px',
     },
-  })
+    fonts: {
+      inter:
+        "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    },
+    radii: {
+      1: '4px',
+      2: '8px',
+      3: '16px',
+      pill: '20%',
+      round: '50%',
+    },
+    sizes: {
+      16: '16px',
+      24: '24px',
+      32: '32px',
+      4: '4px',
+      40: '40px',
+      48: '48px',
+      64: '64px',
+      72: '64px',
+      8: '8px',
+      80: '80px',
+    },
+    space: {
+      120: '120px',
+      16: '16px',
+      24: '24px',
+      32: '32px',
+      4: '4px',
+      40: '40px',
+      48: '48px',
+      64: '64px',
+      8: '8px',
+      80: '80px',
+    },
+    zIndices: {
+      1: '100',
+      2: '200',
+      3: '300',
+      4: '400',
+      5: '500',
+      6: '600',
+      7: '700',
+      8: '800',
+      9: '900',
+      max: '9999',
+    },
+  },
+})
 
 type ColorMode = 'light' | 'dark'
 
@@ -175,7 +174,6 @@ export {
   css,
   darkTheme,
   getCssText,
-  globalCss,
   keyframes,
   lightTheme,
   styled,
