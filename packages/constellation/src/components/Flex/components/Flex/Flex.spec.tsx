97b21e13ff343d2f244bbc9d3299c0061f9e2037
Andreas Wiermann Casas <awiermanncasas@blockchain.com>
2022-04-01 15:28:00 -0300

    test(Flex): add flex component to surface the flex api

---
+++ packages/constellation/src/components/Flex/components/Flex/Flex.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Flex } from '.'
+
+describe('Flex', () => {
+  it('Should create the flex element with the correct flex parameters', () => {
+    // const component = mount(
+    //   <Flex gap={12} flexDirection='column' alignItems='center' justifyContent='space-between' />
+    // )
+    // expect(component).toHaveStyleRule('display', 'flex')
+    // expect(component).toHaveStyleRule('gap', '12px')
+    // expect(component).toHaveStyleRule('flex-direction', 'column')
+    // expect(component).toHaveStyleRule('align-items', 'center')
+    // expect(component).toHaveStyleRule('justify-content', 'space-between')
+  })
+})
---
+++ packages/constellation/src/components/Flex/components/Flex/Flex.tsx
@@ -0,0 +1,6 @@
+import React, { CSSProperties, useMemo } from 'react'
+
+// import styled, { css } from 'styled-components'
+import { FlexComponent, FlexProps } from './types'
+
+export const Flex: FlexComponent = () => null
