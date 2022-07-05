52cbcb65a188a5ad1a3826e0f65472986a45883a
noahub <noah.matsell@metalabdesign.com>
2022-07-05 16:51:36 -0700

    pin version numbers, update slider imports

--- packages/constellation/src/components/Compositions/Slider/Slider.tsx
+++ packages/constellation/src/components/Compositions/Slider/Slider.tsx
@@ -1,11 +1,11 @@
-import * as SliderPrimitive from '@radix-ui/react-slider'
+import { Range, Root, Thumb, Track } from '@radix-ui/react-slider'
 import React from 'react'
 
 import { SliderComponent } from './Slider.types'
 
 const Slider: SliderComponent = ({ max, min, step = 1, value, ...props }) => {
   return (
-    <SliderPrimitive.Root
+    <Root
       value={value}
       step={step}
       min={min}
@@ -14,11 +14,11 @@ const Slider: SliderComponent = ({ max, min, step = 1, value, ...props }) => {
       aria-label='value'
       className='relative flex h-5 w-64 touch-none items-center'
     >
-      <SliderPrimitive.Track className='relative h-1 w-full grow rounded-full bg-medium'>
-        <SliderPrimitive.Range className='absolute h-full rounded-full bg-primary' />
-      </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className='block h-5 w-5 rounded-full bg-background border-background-light focus:outline-none drop-shadow-control theme-dark:bg-dark-500' />
-    </SliderPrimitive.Root>
+      <Track className='relative h-1 w-full grow rounded-full bg-medium'>
+        <Range className='absolute h-full rounded-full bg-primary' />
+      </Track>
+      <Thumb className='block h-5 w-5 rounded-full bg-background border-background-light focus:outline-none drop-shadow-control theme-dark:bg-dark-500' />
+    </Root>
   )
 }
 
