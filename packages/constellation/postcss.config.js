b6555a4305e9e1ba483cabe6f56a020e940306ec
noahub <noah.matsell@metalabdesign.com>
2022-07-05 17:53:18 -0700

    update postcss config format

--- packages/constellation/postcss.config.js
+++ packages/constellation/postcss.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
 /* eslint-disable  */
-const cssnano = require('cssnano')
 
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production' ? cssnano({ preset: 'default' }) : null,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  },
 }
