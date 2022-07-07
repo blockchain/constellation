6836b16322c506fb5deb34721b4f8447178960b1
Ethan Coward <ethanjcoward@gmail.com>
2022-07-07 12:36:54 -0700

    Add theme context provider and stop using wrapping div

--- packages/constellation/src/components/ThemeProvider/ThemeProvider.tsx
+++ packages/constellation/src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,26 +1,33 @@
 /* eslint-disable import/no-unresolved */
 import '../styles.css'
 
-import React, { CSSProperties, useMemo } from 'react'
+import React, { useEffect } from 'react'
 
+import { DarkModeContext } from './themeContext'
 import { ThemeProviderComponent } from './ThemeProvider.types'
 
 const ThemeProvider: ThemeProviderComponent = ({ children, theme, colorOverride = {} }) => {
-  const colorOverrideStyles = useMemo((): CSSProperties => {
-    return Object.keys(colorOverride).reduce(
-      (newColorOverride, color) => ({
-        ...newColorOverride,
-        [`--color-${color}`]: colorOverride[color as keyof typeof colorOverride],
-      }),
-      {},
-    )
-  }, [colorOverride])
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('theme-dark')
+      document.body.classList.remove('theme-light')
+    } else {
+      document.body.classList.add('theme-light')
+      document.body.classList.remove('theme-dark')
+    }
 
-  return (
-    <div className={theme === 'dark' ? 'theme-dark' : 'theme-light'} style={colorOverrideStyles}>
-      {children}
-    </div>
-  )
+    const colorOverrideStyles = Object.keys(colorOverride).reduce(
+      (newColorOverride, color) =>
+        `${newColorOverride} --color-${color}: ${
+          colorOverride[color as keyof typeof colorOverride]
+        };`,
+      '',
+    )
+
+    document.body.style.cssText = colorOverrideStyles
+  }, [colorOverride, theme])
+
+  return <DarkModeContext.Provider value={theme === 'dark'}>{children}</DarkModeContext.Provider>
 }
 
 export default ThemeProvider
