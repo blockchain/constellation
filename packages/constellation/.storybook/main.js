96a278c49e8101bf592ee38bbc6872abac7e9a2f
noahub <noah.matsell@metalabdesign.com>
2022-07-11 11:29:33 -0700

    cleanup

--- packages/constellation/.storybook/main.js
+++ packages/constellation/.storybook/main.js
@@ -24,9 +24,6 @@ module.exports = {
   webpackFinal: (config) => {
     // Storybook includes a loader rule for handling SVGs that conflicts with
     // the `@svgr/webpack` rule we want to add to load SVGs as React components.
-    //
-    // First, we exclude icon svgs from Storybook's existing rules, and then
-    // add a rule that mirrors the one used by Next.js's webpack config.
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test('.svg'))
     const pathToInlineSvg = path.resolve(__dirname, '../src/components/Base/Icon/icons')
     fileLoaderRule.exclude = pathToInlineSvg
