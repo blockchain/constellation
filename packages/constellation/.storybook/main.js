09858ac2dc81f53bc17c326259b0e13c0ef2abe6
noahub <noah.matsell@metalabdesign.com>
2022-07-11 11:24:37 -0700

    Add Icons to Component Library

--- packages/constellation/.storybook/main.js
+++ packages/constellation/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -16,4 +18,25 @@ module.exports = {
     },
   ],
   framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: (config) => {
+    // Storybook includes a loader rule for handling SVGs that conflicts with
+    // the `@svgr/webpack` rule we want to add to load SVGs as React components.
+    //
+    // First, we exclude icon svgs from Storybook's existing rules, and then
+    // add a rule that mirrors the one used by Next.js's webpack config.
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test('.svg'))
+    const pathToInlineSvg = path.resolve(__dirname, '../src/components/Base/Icon/icons')
+    fileLoaderRule.exclude = pathToInlineSvg
+
+    config.module.rules.push({
+      include: path.resolve(__dirname, '../src/'),
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
---
+++ packages/constellation/src/components/Base/Icon/Icon.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import * as Icons from './Icon'
+import type { Props } from './Icon.types'
+
+export const Icon = ({ icon = 'IconPlus', ...args }: Props & { icon: keyof typeof Icons }) => {
+  const RenderedIcon = Icons[icon]
+
+  return <RenderedIcon {...args} />
+}
+
+export const AllIcons = () => (
+  <div className='flex flex-wrap items-start gap-4'>
+    {Object.keys(Icons).map((name) => {
+      const Component = Icons[name as keyof typeof Icons] as React.ComponentType
+
+      return (
+        <div className='flex items-center gap-1 w-40' key={name}>
+          <Component />
+          <p className='text-xs'>{name}</p>
+        </div>
+      )
+    })}
+  </div>
+)
+
+export default {
+  argTypes: {
+    color: {
+      control: { type: 'select' },
+      options: ['#999999'],
+    },
+    icon: {
+      control: { type: 'select' },
+      options: Object.keys(Icons),
+    },
+    size: { control: { type: 'number' } },
+  },
+  args: {},
+  component: Icons.IconPlus,
+  title: 'Base/Icon',
+}
