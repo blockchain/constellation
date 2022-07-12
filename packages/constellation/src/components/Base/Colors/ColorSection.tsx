5ee6815f2543aef1b7338d2d70898dcbe16d1576
Ethan Coward <ethanjcoward@gmail.com>
2022-07-12 13:58:13 -0700

    Update ColorSection.tsx

--- packages/constellation/src/components/Base/Colors/ColorSection.tsx
+++ packages/constellation/src/components/Base/Colors/ColorSection.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+
 import Color from './Color'
 
 const ColorSection = ({ colors, title }: { colors: [string]; title: string }) => {
   return (
     <div className='max-w-4xl flex-shrink-0'>
-      <h2>{title}</h2>
+      <h2 className='text-title'>{title}</h2>
 
       <div className='flex flex-wrap gap-4 mt-5'>
         {colors?.map((color, index) => (
