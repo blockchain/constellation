d9f30816c197c6e9a3263dcc5b92bdc4e03f6f05
noahub <noah.matsell@metalabdesign.com>
2022-07-15 10:58:59 -0700

    Add Link Component

---
+++ packages/constellation/src/components/Compositions/Buttons/Button/index.ts
@@ -0,0 +1,2 @@
+export { default as Button } from './Button'
+export type { Component, Props } from './Button.types'
---
+++ packages/constellation/src/components/Compositions/Buttons/Link/index.ts
@@ -0,0 +1,2 @@
+export { default as Link } from './Link'
+export type { Component, Props } from './Link.types'
