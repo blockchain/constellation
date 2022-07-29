e1e74e3427d438fe59a1efa04da8d56bbb13dfa9
Ethan Coward <ethanjcoward@gmail.com>
2022-07-29 13:26:09 -0700

    Remove the style props form all storybook entries

--- packages/constellation/src/components/Base/FlexGrow/FlexGrow.stories.tsx
+++ packages/constellation/src/components/Base/FlexGrow/FlexGrow.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React, { FC } from 'react'
 
-import { Flex, Padding } from '..'
+import { Flex, Padding } from '../..'
 import { FlexGrow, FlexGrowComponent } from '.'
 
 export default {
@@ -46,94 +46,116 @@ export default {
 } as ComponentMeta<FlexGrowComponent>
 
 export const GrowElementVertically = () => (
-  <Flex flexDirection='column' grow style={{ height: 600, width: 300 }}>
-    <Padding all={1} style={{ background: 'red', color: 'white' }}>
-      Header
-    </Padding>
+  <div className='constellation w-96 h-72 flex'>
+    <Flex flexDirection='column'>
+      <div className='bg-red-500'>
+        <Padding all={1}>Header</Padding>
+      </div>
 
-    <FlexGrow grow style={{ background: 'green', color: 'white' }}>
-      <Padding all={1}>Content</Padding>
-    </FlexGrow>
+      <FlexGrow grow>
+        <div className='bg-green-500 h-full'>
+          <Padding all={1}>Content</Padding>
+        </div>
+      </FlexGrow>
 
-    <Padding all={1} style={{ background: 'blue', color: 'white' }}>
-      Footer
-    </Padding>
-  </Flex>
+      <div className='bg-blue-500'>
+        <Padding all={1}>Footer</Padding>
+      </div>
+    </Flex>
+  </div>
 )
 
 export const GrowMultipleElementsVertically = () => (
-  <Flex flexDirection='column' style={{ height: 600, width: 300 }}>
-    <Padding all={1} style={{ background: 'red', color: 'white' }}>
-      Header
-    </Padding>
+  <div className='constellation w-96 h-72 flex'>
+    <Flex flexDirection='column'>
+      <div className='bg-red-500'>
+        <Padding all={1}>Header</Padding>
+      </div>
 
-    <FlexGrow grow style={{ background: 'green', color: 'white' }}>
-      <Padding all={1}>Content</Padding>
-    </FlexGrow>
+      <FlexGrow grow>
+        <div className='bg-green-500 h-full'>
+          <Padding all={1}>Content</Padding>
+        </div>
+      </FlexGrow>
 
-    <FlexGrow grow style={{ background: 'blue', color: 'white' }}>
-      <Padding all={1}>Footer</Padding>
-    </FlexGrow>
-  </Flex>
+      <FlexGrow grow>
+        <div className='bg-blue-500 h-full'>
+          <Padding all={1}>Footer</Padding>
+        </div>
+      </FlexGrow>
+    </Flex>
+  </div>
 )
 
 export const GrowElementHorizontally = () => (
-  <Flex style={{ height: 600, width: 800 }}>
-    <Padding all={1} style={{ background: 'red', color: 'white' }}>
-      Column 1
-    </Padding>
+  <div className='constellation w-96 h-72 flex'>
+    <Flex flexDirection='row' grow>
+      <div className='bg-red-500'>
+        <Padding all={1}>Header</Padding>
+      </div>
 
-    <FlexGrow grow style={{ background: 'green', color: 'white' }}>
-      <Padding all={1}>Column 2</Padding>
-    </FlexGrow>
+      <FlexGrow grow>
+        <div className='bg-green-500 h-full'>
+          <Padding all={1}>Content</Padding>
+        </div>
+      </FlexGrow>
 
-    <Padding all={1} style={{ background: 'blue', color: 'white' }}>
-      Column 3
-    </Padding>
-  </Flex>
+      <div className='bg-blue-500 h-full'>
+        <Padding all={1}>Footer</Padding>
+      </div>
+    </Flex>
+  </div>
 )
 
 export const GrowMultipleElementsHorizontally = () => (
-  <Flex grow style={{ height: 600, width: 800 }}>
-    <Padding all={1} style={{ background: 'red', color: 'white' }}>
-      Column 1
-    </Padding>
+  <div className='constellation w-96 h-72 flex'>
+    <Flex flexDirection='row' grow>
+      <div className='bg-red-500'>
+        <Padding all={1}>Header</Padding>
+      </div>
 
-    <FlexGrow grow style={{ background: 'green', color: 'white' }}>
-      <Padding all={1}>Column 2</Padding>
-    </FlexGrow>
+      <FlexGrow grow>
+        <div className='bg-green-500 h-full'>
+          <Padding all={1}>Content</Padding>
+        </div>
+      </FlexGrow>
 
-    <FlexGrow grow style={{ background: 'blue', color: 'white' }}>
-      <Padding all={1}>Column 3</Padding>
-    </FlexGrow>
-  </Flex>
+      <FlexGrow grow>
+        <div className='bg-blue-500 h-full'>
+          <Padding all={1}>Footer</Padding>
+        </div>
+      </FlexGrow>
+    </Flex>
+  </div>
 )
 
 export const ShrinkElementHorizontally = () => (
-  <Flex style={{ height: 600, width: 800 }}>
-    <FlexGrow shrink={0} style={{ background: 'red', color: 'white' }}>
-      <Padding all={1}>
-        <b>This will not shrink at all.</b>
-      </Padding>
-    </FlexGrow>
+  <div className='constellation w-[1000px] h-72 flex'>
+    <Flex grow>
+      <FlexGrow shrink={0}>
+        <Padding all={1}>
+          <b>This will not shrink at all.</b>
+        </Padding>
+      </FlexGrow>
 
-    <FlexGrow shrink={2} style={{ background: 'green', color: 'white' }}>
-      <Padding all={1}>
-        <b>This will shrink.</b> <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nemo, exercitationem non, iste cum cumque sed expedita suscipit consectetur pariatur
-        laudantium eum sit consequatur? Incidunt natus quia at doloribus minus repudiandae!
-      </Padding>
-    </FlexGrow>
+      <FlexGrow shrink={2}>
+        <Padding all={1}>
+          <b>This will shrink.</b> <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nemo, exercitationem non, iste cum cumque sed expedita suscipit consectetur pariatur
+          laudantium eum sit consequatur? Incidunt natus quia at doloribus minus repudiandae!
+        </Padding>
+      </FlexGrow>
 
-    <FlexGrow grow style={{ background: 'blue', color: 'white' }}>
-      <Padding all={1}>
-        <b>This will grow.</b> <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam commodi, vel quos hic sit
-        culpa, voluptate corrupti atque facere perspiciatis rerum ipsum. Temporibus nihil ipsa
-        nostrum adipisci perspiciatis consectetur ipsam.
-      </Padding>
-    </FlexGrow>
-  </Flex>
+      <FlexGrow grow>
+        <Padding all={1}>
+          <b>This will grow.</b> <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam commodi, vel quos hic sit
+          culpa, voluptate corrupti atque facere perspiciatis rerum ipsum. Temporibus nihil ipsa
+          nostrum adipisci perspiciatis consectetur ipsam.
+        </Padding>
+      </FlexGrow>
+    </Flex>
+  </div>
 )
 
 type testProps = {
@@ -155,29 +177,25 @@ export const InteractiveFlexGrowAndShrink: ComponentStory<TestComponent> = ({
   column3Grow,
   column3Shrink,
 }) => (
-  <Flex style={{ height: 600, width: 800 }}>
-    <FlexGrow
-      grow={column1Grow}
-      shrink={column1Shrink}
-      style={{ background: 'red', color: 'white' }}
-    >
-      <Padding all={1}>Column 1</Padding>
-    </FlexGrow>
+  <div className='constellation w-[1000px] h-72 flex'>
+    <Flex grow>
+      <FlexGrow grow={column1Grow} shrink={column1Shrink}>
+        <div className='bg-blue-500 h-full'>
+          <Padding all={1}>Column 1</Padding>
+        </div>
+      </FlexGrow>
 
-    <FlexGrow
-      grow={column2Grow}
-      shrink={column2Shrink}
-      style={{ background: 'green', color: 'white' }}
-    >
-      <Padding all={1}>Column 2</Padding>
-    </FlexGrow>
+      <FlexGrow grow={column2Grow} shrink={column2Shrink}>
+        <div className='bg-red-500 h-full'>
+          <Padding all={1}>Column 2</Padding>
+        </div>
+      </FlexGrow>
 
-    <FlexGrow
-      grow={column3Grow}
-      shrink={column3Shrink}
-      style={{ background: 'blue', color: 'white' }}
-    >
-      <Padding all={1}>Column 3</Padding>
-    </FlexGrow>
-  </Flex>
+      <FlexGrow grow={column3Grow} shrink={column3Shrink}>
+        <div className='bg-green-500 h-full'>
+          <Padding all={1}>Column 3</Padding>
+        </div>
+      </FlexGrow>
+    </Flex>
+  </div>
 )
