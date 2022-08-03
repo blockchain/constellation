cc66737825cd59fbff5ed675471d16e7c4f8f1d2
Ethan Coward <ethanjcoward@gmail.com>
2022-08-03 10:41:42 -0700

    Update Logo.types.ts

--- packages/constellation/src/components/Compositions/Logo/Logo.types.ts
+++ packages/constellation/src/components/Compositions/Logo/Logo.types.ts
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react'
+import { FC, ReactNode } from 'react'
 
 type InternalLogoProps = {
   circle: boolean
@@ -9,9 +9,29 @@ type InternalLogoProps = {
 type InternalLogoComponent = FC<InternalLogoProps>
 
 type LogoProps = {
+  /**
+   * Displays the logo as either a circle or rounded square. Defaults to true.
+   */
   circle?: boolean
+  /**
+   * The content of the primary logo.
+   * If a string is provided, it will be rendered as text in the primary logo.
+   * If a React node is provided, it will be rendered at full width and height.
+   * in the primary logo.
+   */
   primary: string | ReactNode
+  /**
+   * The content of the secondary logo.
+   * If a string is provided, it will be rendered as text in the secondary logo.
+   * If a React node is provided, it will be rendered at full width and height.
+   * in the secondary logo.
+   */
   secondary?: string | ReactNode
+  /**
+   * The style of the secondary logo. Defaults to "primary". If primary then both
+   * logos will be rendered at 24px slightly overlapping. If badge the secondary logo
+   * will be rendered at 16px in the bottom right corner and the primary logo at full size.
+   */
   secondaryVariant?: 'primary' | 'badge'
 }
 
