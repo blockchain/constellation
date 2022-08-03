21d270f2c0fa29a7cad3873cd234d5e770afbcd6
Ethan Coward <ethanjcoward@gmail.com>
2022-08-03 12:37:55 -0700

    Add base Cells

---
+++ packages/constellation/src/components/Compositions/Table/Cell/Cell.types.ts
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from 'react'
+
+type BaseCellProps = {}
+type BaseCellComponent = FC<BaseCellProps>
+
+type TextCellProps = {}
+type TextCellComponent = FC<TextCellProps>
+
+export type { BaseCellComponent, BaseCellProps, TextCellComponent, TextCellProps }
---
+++ packages/constellation/src/components/Compositions/Table/Cell/index.ts
@@ -0,0 +1,7 @@
+export { TextCell } from './Cell'
+export type {
+  BaseCellComponent,
+  BaseCellProps,
+  TextCellComponent,
+  TextCellProps,
+} from './Cell.types'
