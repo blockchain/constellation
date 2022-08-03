d0f6bf77d25f1cabbfa2ee5734001dea5a58ae0f
noahub <noah.matsell@metalabdesign.com>
2022-08-03 12:04:46 -0700

    comments

--- packages/constellation/fileTransformer.ts
+++ packages/constellation/fileTransformer.ts
@@ -1,6 +1,9 @@
 // eslint-disable-next-line
 const path = require('path')
 
+/**
+ * This custom transformer allows Jest/Storyshots to parse Icon svg files without throwing an invalid element error
+ */
 module.exports = {
   process(src, filename) {
     return `module.exports = ${JSON.stringify(path.basename(filename))};`
