4d913956b38d7d2b7b53e61b964235532dbcc5f4
Ethan Coward <ethanjcoward@gmail.com>
2022-08-05 16:33:52 -0700

    Update TextCell.tsx

--- packages/constellation/src/components/Compositions/Table/Cells/TextCell/TextCell.tsx
+++ packages/constellation/src/components/Compositions/Table/Cells/TextCell/TextCell.tsx
@@ -1,13 +1,14 @@
+import cx from 'classnames'
 import React from 'react'
 
 import { BaseCell } from '../'
 import { TextCellComponentType } from '.'
 
 const TextCell: TextCellComponentType = ({
-  isFooter,
-  isHeader,
   icon,
   iconPosition,
+  isFooter,
+  isHeader,
   subtext,
   text,
 }) => {
@@ -20,7 +21,13 @@ const TextCell: TextCellComponentType = ({
     >
       <div className='flex flex-col justify-center'>
         <span className='text-title text-sm'>{text}</span>
-        <span className='text-body !mode-light:text-background-dark text-sm'>{subtext}</span>
+        <span
+          className={cx('text-body mode-light:text-background-dark text-sm', {
+            'mode-light:!text-grey-600 mode-dark:!text-grey-400': isHeader,
+          })}
+        >
+          {subtext}
+        </span>
       </div>
     </BaseCell>
   )
