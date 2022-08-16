3e56e046bfcdc3ef36d4ecaa59bd2e25c3c36372
noahub <noah.matsell@metalabdesign.com>
2022-08-16 11:15:28 -0700

    comments

--- packages/constellation/src/components/Primitives/Divider/Divider.types.ts
+++ packages/constellation/src/components/Primitives/Divider/Divider.types.ts
@@ -5,7 +5,13 @@ export type DividerVariants = 'subtle' | 'medium' | 'distinct'
 export type DividerOrientations = 'horizontal' | 'vertical'
 
 export type Props = {
+  /**
+   * The orientation of the divider
+   */
   orientation?: DividerOrientations
+  /**
+   * The stylistic variant of the divider
+   */
   variant?: DividerVariants
 } & SeparatorProps
 
