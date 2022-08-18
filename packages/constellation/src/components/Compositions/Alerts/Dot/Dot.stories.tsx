3c3c4748ea3bac89059b9f81779c5718a9b55500
Ethan Coward <ethanjcoward@gmail.com>
2022-08-18 10:32:02 -0700

    Add alert dot

---
+++ packages/constellation/src/components/Compositions/Alerts/Dot/Dot.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+
+import { AlertDot, AlertDotComponentType } from '.'
+
+export default {
+  argTypes: {},
+  args: {},
+  component: AlertDot,
+  title: 'Compositions/Alert/Dot',
+} as ComponentMeta<AlertDotComponentType>
+
+const Template: ComponentStory<AlertDotComponentType> = ({ ...args }) => {
+  return <AlertDot {...args} />
+}
+
+export const Dot = Template.bind({})
