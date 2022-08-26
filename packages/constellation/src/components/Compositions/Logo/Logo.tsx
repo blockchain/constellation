c3737af488a4b3698bb25f436ac3c04e9e09f794
Ethan Coward <ethanjcoward@gmail.com>
2022-08-26 16:03:22 -0700

    Add the switcher components (#187)
    * start on switcher
    
    * Update logo sizing
    
    * Add the basic switcher
    
    * add wallet switcher base
    
    * finish walletSwitcher
    
    * fix card logo
    
    * docs and update tests
    
    * Update yarn.lock
    
    * More switcher and make truncateId a util function
    
    * Update storyshots.test.ts.snap

--- packages/constellation/src/components/Compositions/Logo/Logo.tsx
+++ packages/constellation/src/components/Compositions/Logo/Logo.tsx
@@ -14,37 +14,73 @@ const InternalLogo: InternalLogoComponent = ({
   imgSrc,
   size = 'base',
   text,
+  variant = 'base',
 }) => {
   const Icon = icon as FC<IconProps>
 
   const genFontSizes = (
     text?: string,
-  ): { badge: string; base: string; double: string; large: string } => {
+    size?: string,
+  ): { badge: string; base: string; double: string } => {
     const length = text?.length || 4
 
-    return {
-      badge: length <= 4 ? 'text-[3.5px]' : 'text-[2.5px]',
-      base: length <= 4 ? 'text-[10px]' : 'text-[7px]',
-      double: length <= 4 ? 'text-[8px]' : 'text-[6px]',
-      large: length <= 4 ? 'text-[12px]' : 'text-[9px]',
+    switch (size) {
+      case 'large':
+        return {
+          badge: length <= 4 ? 'text-[5px]' : 'text-[3px]',
+          base: length <= 4 ? 'text-[12px]' : 'text-[8px]',
+          double: length <= 4 ? 'text-[9px]' : 'text-[7px]',
+        }
+      case 'small':
+        return {
+          badge: length <= 4 ? 'text-[2.5px]' : 'text-[1.5px]',
+          base: length <= 4 ? 'text-[6px]' : 'text-[4px]',
+          double: length <= 4 ? 'text-[5px]' : 'text-[3.5px]',
+        }
+      default:
+        return {
+          badge: length <= 4 ? 'text-[3.5px]' : 'text-[2.5px]',
+          base: length <= 4 ? 'text-[10px]' : 'text-[7px]',
+          double: length <= 4 ? 'text-[8px]' : 'text-[6px]',
+        }
     }
   }
 
-  const fontSizes = useMemo(() => genFontSizes(text), [text])
+  const fontSizes = useMemo(() => genFontSizes(text, size), [text, size])
 
-  const sizes = {
-    badge: 'w-3 h-3',
-    base: 'w-8 h-8',
-    double: 'w-6 h-6',
-    large: 'w-10 h-10',
+  const genSizes = (
+    singeVariant: 'base' | 'large' | 'small',
+  ): { badge: string; base: string; double: string } => {
+    switch (singeVariant) {
+      case 'large':
+        return {
+          badge: 'w-4 h-4',
+          base: 'w-10 h-10',
+          double: 'w-8 h-8',
+        }
+      case 'small':
+        return {
+          badge: 'w-2 h-2',
+          base: 'w-5 h-5',
+          double: 'w-4 h-4',
+        }
+      default:
+        return {
+          badge: 'w-3 h-3',
+          base: 'w-8 h-8',
+          double: 'w-6 h-6',
+        }
+    }
   }
+
+  const variantStyles = useMemo(() => genSizes(size), [size])
 
   return (
     <Avatar.Root
       className={cx(
         { '!rounded-full': circle },
-        'rounded flex justify-center items-center bg-primary relative overflow-hidden',
-        sizes[size],
+        'rounded flex justify-center items-center bg-primary relative overflow-hidden w-',
+        variantStyles[variant],
         className,
       )}
       style={backgroundColor && { backgroundColor }}
@@ -53,7 +89,7 @@ const InternalLogo: InternalLogoComponent = ({
         <Avatar.Image src={imgSrc} className='w-full h-full object-cover' />
         <Avatar.Fallback>
           {(icon && <Icon color={iconColor} />) || (
-            <div className={cx('text-white-000', fontSizes[size])}>{text}</div>
+            <div className={cx('text-white-000', fontSizes[variant])}>{text}</div>
           )}
         </Avatar.Fallback>
       </>
@@ -66,15 +102,21 @@ const Logo: LogoComponent = ({
   doubleVariant = 'primary',
   primaryContent,
   secondaryContent,
-  singleVariant = 'base',
+  size,
 }) => {
   const badge = doubleVariant === 'badge'
 
   return (
     <div
-      className={cx('constellation w-8 h-10 relative flex justify-center items-center', {
-        '!w-10': singleVariant === 'large' && !secondaryContent,
-      })}
+      className={cx(
+        'constellation w-8 h-10 relative flex justify-center items-center',
+        {
+          '!w-10 h-[52px]': size === 'large',
+        },
+        {
+          '!w-5 !h-8': size === 'small',
+        },
+      )}
     >
       {secondaryContent && (
         <InternalLogo
@@ -84,10 +126,15 @@ const Logo: LogoComponent = ({
           icon={secondaryContent.icon}
           iconColor={secondaryContent.iconColor}
           imgSrc={secondaryContent.imgSrc}
-          size={badge ? 'badge' : 'double'}
-          className={cx('!absolute bottom-0 right-0', {
-            '!w-3 !h-3 border-2 border-background border-solid -mr-[2px] mb-[2px] z-10': badge,
-          })}
+          variant={badge ? 'badge' : 'double'}
+          size={size}
+          className={cx(
+            '!absolute bottom-0 right-0',
+            {
+              'border-2 border-background border-solid -mr-[2px] mb-[2px] z-10': badge,
+            },
+            { '!border !m-0 !mb-1': size === 'small' },
+          )}
         />
       )}
       <InternalLogo
@@ -97,7 +144,8 @@ const Logo: LogoComponent = ({
         icon={primaryContent.icon}
         iconColor={primaryContent.iconColor}
         imgSrc={primaryContent.imgSrc}
-        size={!secondaryContent ? singleVariant : badge ? 'base' : 'double'}
+        variant={badge || !secondaryContent ? 'base' : 'double'}
+        size={size}
         className={cx({
           '!absolute top-0 left-0 border-2 border-background border-solid -ml-[2px] -mt-[2px]':
             secondaryContent && !badge,
