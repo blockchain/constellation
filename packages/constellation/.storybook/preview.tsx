561dcf3b854d0d43dfd36bf026984e7f9bd4225f
Ethan Coward <ethanjcoward@gmail.com>
2022-08-31 12:25:39 -0700

    Fix dark mode in docs view

--- packages/constellation/.storybook/preview.tsx
+++ packages/constellation/.storybook/preview.tsx
@@ -3,12 +3,45 @@ import React from 'react'
 import { ThemeProvider } from '../src'
 import { useDarkMode } from 'storybook-dark-mode'
 import { themes } from '@storybook/theming'
+import { DocsContainer as BaseContainer } from '@storybook/addon-docs/blocks'
 import '../src/input.css'
+
+export const DocsContainer = ({ children, context }) => {
+  const dark = useDarkMode()
+
+  return (
+    <BaseContainer
+      context={{
+        ...context,
+        storyById: (id) => {
+          const storyContext = context.storyById(id)
+          return {
+            ...storyContext,
+            parameters: {
+              ...storyContext?.parameters,
+              docs: {
+                ...storyContext?.parameters?.docs,
+                theme: dark
+                  ? { ...themes.dark, appContentBg: '#2f2f2f' }
+                  : { ...themes.normal, appContentBg: '#FFFFFF' },
+              },
+            },
+          }
+        },
+      }}
+    >
+      {children}
+    </BaseContainer>
+  )
+}
 
 export const parameters = {
   darkMode: {
     dark: { ...themes.dark, appContentBg: '#2f2f2f' },
     light: { ...themes.normal, appContentBg: '#FFFFFF' },
+  },
+  docs: {
+    container: DocsContainer,
   },
 }
 
